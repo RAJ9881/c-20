624e93fe46d5be0d00024078452886532b350678
RAJ9881 <70510967+RAJ9881@users.noreply.github.com>
2020-09-01 14:01:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+
+function setup()
+ { createCanvas(1200,800); 
+  fixedRect=createSprite(200, 200, 50, 80);
+   movingRect=createSprite(400,200,80,30);
+    fixedRect.shapeColor="green";
+     movingRect.shapeColor="green";
+      fixedRect.debug='true'; 
+      movingRect.debug='true'; }
+      
+      
+      
+      
+      
+      function draw() 
+      { background(0);
+         movingRect.x=World.mouseX; 
+         movingRect.y=World.mouseY;
+          if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2)
+          { movingRect.shapeColor="red"; 
+          fixedRect.shapeColor="red";
+         }else{ movingRect.shapeColor="green";
+          fixedRect.shapeColor="green"; } 
+          drawSprites();
+         }
